bb83cf327e08c2a6a75977ff9e7f1f56eef3c23a
Esti Shay and Stephanie Mayer <stephanie@>
2017-08-03 13:39:41 -0700

    create test to instantiate correctly

--- src/main/java/models/Dice.java
+++ src/main/java/models/Dice.java
@@ -1,7 +1,12 @@
 package models;
 
-/**
- * Created by Guest on 8/3/17.
- */
 public class Dice {
+    public int randNum;
+
+    public Dice (){
+        this.randNum = 0;
+    }
+
+
+
 }
--- src/test/java/models/DiceTest.java
+++ src/test/java/models/DiceTest.java
@@ -1,8 +1,16 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 
 public class DiceTest {
+
+    @Test
+    public void Dice_createsDie_instantiatesCorrectly() throws Exception {
+        Dice testDice = new Dice();
+        assertEquals(true, testDice instanceof Dice);
+    }
 
 }
